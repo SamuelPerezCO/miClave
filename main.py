af4f09189d7609e2d22103e579f2e4fb118a9ab5
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-04-13 13:21:47 -0500

    Primeras lineas de codigo

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Hello world")
+import customtkinter
+from pagina_principal import mostrar_pagina_principal
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    mostrar_pagina_principal()
+    root.destroy()
+
+frame = customtkinter.CTkFrame(master=root)
+
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="miClave")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Usuario")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Contraseña", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Iniciar Sesion", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Recordarme")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
---
+++ pagina_principal.py
@@ -0,0 +1,12 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+def mostrar_pagina_principal():
+    root = customtkinter.CTk()
+    root.geometry("1000x700")
+
+    # Coloca aquí los widgets de tu página principal
+
+    root.mainloop()
